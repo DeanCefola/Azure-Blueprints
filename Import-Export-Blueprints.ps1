16d3293511a68c669dff3be9d43f93f8136b6b46
Dean Cefola <Dean.Cefola@microsoft.com>
2019-06-26 11:33:28 -0400

    add management group support
    Add export and import commands for management groups and subscriptions

--- Import-Export-Blueprints.ps1
+++ Import-Export-Blueprints.ps1
@@ -7,15 +7,16 @@
 #------------------------------------------------------------------------------
 # 05/20/2019                       1.0       Intial Version
 # 06/25/2019                       2.0       Convert to Az.Blueprint modules
+# 06/26/2019                       3.0       Add Management Group & Subscription Code
 #
 #******************************************************************************
 #
 #>
 
 
-##############################
-#    Get Subscription Info   #
-##############################
+########################
+#    Get Azure  Info   #
+########################
 $MgmtName = '<ENTER MANAGEMENT GROUP NAME>'
 $SubName = '<ENTER SUBSCRIPTION NAME>'
 $SubID = (Get-AzSubscription `
@@ -36,9 +37,9 @@ Import-Module `
     -Name Az.Blueprint
 
 
-##########################
-#    Export Blueprint    #
-##########################
+################################################
+#    Export Blueprint from Management Group    #
+################################################
 $BPName = 'Test'
 $LocalPath = 'C:\temp\Blueprint'
 $BP = Get-AzBlueprint -ManagementGroupId $MgmtID | Where-Object -Property Name -EQ $BPName
@@ -51,10 +52,38 @@ Export-AzBlueprintWithArtifact `
     -Verbose
 
 
-##########################
-#    Import Blueprint    #
-##########################
-$LocalPath='C:\temp\Blueprint\AMP'
+##############################################
+#    Import Blueprint to Management Group    #
+##############################################
+$LocalPath='C:\temp\Blueprint\Test'
+$BPName = $LocalPath.Split('\') | select -Last 1
+Import-AzBlueprintWithArtifact `
+    -Name $BPName `
+    -InputPath $LocalPath `
+    -ManagementGroupId $MgmtID `
+    -Force `
+    -Verbose
+	
+	
+############################################
+#    Export Blueprint from Subscription    #
+############################################
+$BPName = 'Test'
+$LocalPath = 'C:\temp\Blueprint'
+$BP = Get-AzBlueprint -SubscriptionId $SubID | Where-Object -Property Name -EQ $BPName
+$BPVersion = $BP.Versions | select -Last 1
+Export-AzBlueprintWithArtifact `
+    -Blueprint $BP `
+    -OutputPath $LocalPath `
+    -Version $BPVersion `
+    -Force `
+    -Verbose
+
+
+##########################################
+#    Import Blueprint to Subscription    #
+##########################################
+$LocalPath='C:\temp\Blueprint\Test'
 $BPName = $LocalPath.Split('\') | select -Last 1
 Import-AzBlueprintWithArtifact `
     -Name $BPName `
@@ -62,3 +91,6 @@ Import-AzBlueprintWithArtifact `
     -SubscriptionId $SubID `
     -Force `
     -Verbose
+
+
+#
